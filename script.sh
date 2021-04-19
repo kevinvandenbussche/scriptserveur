49e3f9fcbd348e1fd5a7435d750bbee12c4b9d7c
root <root@vps-1a088992.vps.ovh.net>
2021-04-19 10:15:00 +0000

    amelioration script

--- script.sh
+++ script.sh
@@ -9,29 +9,53 @@ snap install core
 snap refresh core
 snap install --classic certbot
 
-CERTBOT = $(ls /usr/bin |grep certbot)
+CERTBOT=$(ls /usr/bin | grep certbot)
 if [ -z "$CERTBOT" ]
 then
-	echo"on creé le lien /usr/bin/certbot"
+	echo "on creé le lien /usr/bin/certbot"
 	ln -s /snap/bin/certbot /usr/bin/certbot
-	fi
+fi
+
+mkdir -p /var/www/html
+
+cd /var/www/html
+
+echo "telechargement de git"
+GIT_CMD=$(ls -lsa /var/www/html | grep .git)
+if [ -z "$GIT_CMD" ]
+
+then
+	git init 
+	git remote add origin https://github.com/kevinvandenbussche/maraudeurs.git
+fi
+
+
+ 
+cd /opt/scriptserveur
+
+APACHE_CHECK=$(ls /etc/apache2/sites-available/ | grep 000-default.conf)
+if [ -z "$APACHE_CHECK" ]
+
+then
+	touch /etc/apache2/sites-available/000-default.conf
+fi
+
 
 MD5_DEST=$(md5sum /etc/apache2/sites-available/000-default.conf | awk '{print $1}')
 MD5_SRC=$(md5sum 000-default.conf | awk '{print $1}')
 
 if [ "$MD5_DEST" != "$MD5_SRC" ]
-then
-	        echo "on ecrase la conf apache"
-		 cp 000-default.conf /etc/apache2/sites-avaibles/000-default.conf
-		  service apache2 restart
-		          fi
+then 
+ 	echo "on ecrase la conf apache"
+	cp 000-default.conf /etc/apache2/sites-available/000-default.conf
+	service apache2 restart
+fi
 
-echo 'pull sources git'
+echo "pull sources git"
 
 cd /var/www/html
-
+git pull origin develop
 composer install
 chown -R www-data:www-data /var/www/html/
 source .env.dev
 echo "penser a mettre la certif apache"
-git pull origin master
